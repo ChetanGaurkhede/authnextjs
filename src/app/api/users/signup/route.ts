7b3b50018b7249327c928e6f7da2a0e32a71dd30
Chetan Gaurkhede <gaurkhede732@gmail.com>
2025-06-27 13:23:50 +0530

    authentication added for user resistretion

---
+++ src/app/api/users/signup/route.ts
@@ -0,0 +1,65 @@
+import { connect } from "@/dbConfig/dbConfig";
+import mongoose from "mongoose";
+import User from "@/model/userModel";
+import { NextRequest, NextResponse } from "next/server";
+import bcrypt from "bcryptjs"; //using for increaption of password
+import { sendEmail } from "@/helpers/mailer";
+
+connect();
+
+export async function POST(request: NextRequest) {
+  try {
+    const reqBody = await request.json();
+
+    const { username, email, Password } = reqBody;
+    console.log(reqBody);
+
+    const user = await User.findOne({ email });
+
+    if (user) {
+      return NextResponse.json(
+        { error: "user already exist" },
+        { status: 400 }
+      );
+    }
+
+    //hashing password
+
+    const salt = await bcrypt.genSaltSync(10);
+
+    const hashedPassword = await bcrypt.hash(Password, salt);
+
+    // adding new user to db
+    const newUser = new User({
+      username,
+      email,
+      Password: hashedPassword,
+    });
+
+    const savedUser = newUser.save();
+    console.log(savedUser);
+
+
+    savedUser.then(() => {
+      console.log("new user added: ", newUser);
+    })
+    .catch((error: any) => {
+      console.log({ error: error.message });
+    });
+
+    // mailer
+
+    await sendEmail({email, emailType: "VERIFY", userId: savedUser._id})
+
+    return NextResponse.json({
+      message: "User register successfully",
+      success: true,
+      savedUser
+    })
+
+
+
+  } catch (error: any) {
+    return NextResponse.json({ error: error.message }, { status: 500 });
+  }
+}
